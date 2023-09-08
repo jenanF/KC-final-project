39cb2ec578c7ee51fe710796d4722a8e41fe5feb
jenanF <108689302+jenanF@users.noreply.github.com>
2023-09-08 04:08:41 +0300

    .

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
+
+Window.size = (400, 630)
+Window.clearcolor= "4b548b"
 
 class MainWidget(Widget):
     pass
 
 class MyAppApp(App):
     pass
+
+def on_toggle_active(self):
+    print("hello")
+
 
 MyAppApp().run()

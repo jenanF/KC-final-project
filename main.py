0b5d94cf7734efd57255d46e2674ef8917de2c21
jenanF <108689302+jenanF@users.noreply.github.com>
2023-09-09 04:25:46 +0300

    .

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.properties import ListProperty, StringProperty
 
 Window.size = (400, 630)
 Window.clearcolor= "4b548b"
@@ -20,15 +21,43 @@ class UkScreen(Screen):
     pass
 
 
+currencies = requests.get("https://www.bloomberg.com/markets/currencies")
+
+def currncy(page):
+    src = page.content
+    soup = BeautifulSoup(src, "lxml")
+    test = soup.find('tbody', {'class' : 'data-table-body'}).text
+    title_curncy = soup.find('th', {'class' : 'data-table-headers-cell'}).text
+    print(title_curncy)
+    print(test.__len__())
+
+
+currncy(currencies)
+
+
+class CurrnciesScreen(Screen):
+    src = currencies.content
+    soup = BeautifulSoup(src, "lxml")
+    test = soup.find('tbody', {'class' : 'data-table-body'}).text
+    title_curncy = StringProperty(soup.find('th', {'class' : 'data-table-headers-cell'}).text)
+    title_value = soup.find_all('tr', {'class' : 'data-table-headers'})[0].children
+    print(title_value)
+
+class ConvertorScreen(Screen):
+    from_ = input("from ").upper()
+    to_ = input("to ").upper()
+    amount = float(input("amount "))
+    convert = requests.get(f"https://api.frankfurter.app/latest?amount={amount}&from={from_}&to={to_}")
+    print(f"{amount}  {from_} is {convert.json()['rates'][to_]}  {to_}")
+
+
+
 
 class ScreenManager(ScreenManager):
     pass
 
 class MyAppApp(App):
     pass
-
-def on_toggle_active(self):
-    print("hello")
 
 
 MyAppApp().run()

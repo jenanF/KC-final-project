2b96246d0379069ba3e48b60b4afd6c597d219a4
jenanF <108689302+jenanF@users.noreply.github.com>
2023-09-11 05:02:59 +0300

    .

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ from kivy.uix.image import Image
 from kivy.properties import ListProperty, StringProperty
 from kivymd.uix.pickers import MDDatePicker
 from kivy.uix.tabbedpanel import TabbedPanel
+import webbrowser
+import speech_recognition as sp
+import pyttsx3
+
+
+
 
 Window.size = (400, 630)
 Window.clearcolor= "4b548b"
@@ -71,9 +77,32 @@ class BritishAirwaysScreen(Screen):
 class UniUkScreen(Screen): 
     pass
 
-class OxfordScreen(Screen):
+class OxfordScreen(Screen):#done
     oxftext = '''The University of Oxford is a collegiate research university in Oxford, England.There is evidence of teaching as early as 1096,making it the oldest university in the English-speaking world and the world's second-oldest university in continuous operation.It grew rapidly from 1167 when Henry II banned English students from attending the University of Paris.After disputes between students and Oxford townsfolk in 1209, some academics fled north-east to Cambridge where they established what became the University of Cambridge.The two English ancient universities share many common features and are jointly referred to as Oxbridge. The university is made up of thirty-nine semi-autonomous constituent colleges, four permanent private halls, and a range of academic departments which are organised into four divisions.All the colleges are self-governing institutions within the university, each controlling its own membership and with its own internal structure and activities. All students are members of a college. It does not have a main campus, and its buildings and facilities are scattered throughout the city centre. Undergraduate teaching at Oxford consists of lectures, small-group tutorials at the colleges and halls, seminars, laboratory work and occasionally further tutorials provided by the central university faculties and departments. Postgraduate teaching is provided in a predominantly centralized fashion. Oxford operates the Ashmolean Museum, the world's oldest university museum; Oxford University Press, the largest university press in the world; and the largest academic library system nationwide.In the fiscal year ending 31 July 2022, the university had a total consolidated income of £2.78 billion, of which £711.4 million was from research grants and contracts. Oxford has educated a wide range of notable alumni, including 30 prime ministers of the United Kingdom and many heads of state and government around the world.As of October 2022, 73 Nobel Prize laureates, 4 Fields Medalists, and 6 Turing Award winners have matriculated, worked, or held visiting fellowships at the University of Oxford, while its alumni have won 160 Olympic medals. Oxford is the home of numerous scholarships, including the Rhodes Scholarship, one of the oldest international graduate scholarship programmes.'''
+    def openweb():
+      webbrowser.open("https://goo.gl/maps/u7sv4fhHwx26Lpnd8")
 
+
+class SiriScreen(Screen):
+    listener = sp.Recognizer()
+    audio = pyttsx3.init()
+    voices = audio.getProperty('voices')
+    audio.setProperty('voice', voices[1].id)
+    def sayit(text):
+        audio = pyttsx3.init()
+        audio.say(text)
+        audio.runAndWait()
+    try:
+        with sp.Microphone() as source:
+            print("go ahead...")
+            voicee = listener.listen(source)
+            request = listener.recognize_google(voicee)
+            print(request)
+            sayit(request)
+            audio.say(request)
+            
+    except:
+        pass
 
 
 class ScreenManager(ScreenManager):
---
+++ test.py
@@ -0,0 +1,131 @@
+import time
+import speech_recognition as sp
+#import playsound
+#import os
+import openai
+import pyttsx3
+
+
+openai.api_key = "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"
+
+audioo = pyttsx3.init()
+
+def audioTotext(name):
+    listener = sp.Recognizer()
+
+    with sp.AudioFile(name) as source:
+        audio = listener.record(source)
+    try:
+        return listener.recognize_google(audio)
+    except:
+        print("unknown error")
+
+def answer(question):
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt=question,
+        max_tokens=4000,
+        n=1,
+       stop=None,
+       temperature=0.5    
+    )
+    return response['choices'][0]['text']
+
+def speakit(text):
+    audioo.say(text)
+    audioo.runAndWait()
+
+def main():
+    while True:
+        print("say (hello) and go ahead...")
+        with sp.Microphone() as source:
+            listener = sp.Recognizer()
+            audio = listener.listen(source)
+            try:
+                script = listener.recognize_google(audio)
+                if script.lower() == 'hello':
+                    name = 'input.wav'
+                    print('go ahead im listening...')
+                    with sp.Microphone() as source:
+                        listener = sp.Recognizer()
+                        source.pause_threshold = 1
+                        audio = listener.listen(source, phrase_time_limit=None, timeout=None)
+                        with open(name, 'wb') as f:
+                            f.write(audio.get_wav_data())
+
+                    text = audioTotext(name)
+                    if text:
+                        print('you said:', text )
+
+                        response = answer(text)
+                        print('Hello said:', response)
+
+                        speakit(response)
+                elif text == 'stop':
+                    break
+
+            except Exception as e:
+                print('error: {}'.format(e))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+lang = 'en'
+openai.api_key = "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"
+
+userr =""
+
+while True:
+    def get_req():
+        print('go ahead...')
+        pyttsx3.init()
+        listener = sp.Recognizer()
+        with sp.Microphone() as source:
+            request = listener.listen(source)
+            said = ""
+
+            try:
+                said = listener.recognize_google(request)
+                print(said)
+                global userr
+                userr = said
+
+                if "hello" in said:
+                    words = said.split()
+                    new = ' '.join(words[1:])
+                    print(new)
+                    complet = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{'role': 'user', 'content': said}])
+                    text = complet.choices[0].message.content
+                    speech = pyttsx3.say(text)
+                    speech.save('welcome1.mp3')
+
+            except Exception:
+              print("exception")
+
+        return said
+    
+    if 'stop' in userr:
+        break
+
+
+get_req()
+'''
